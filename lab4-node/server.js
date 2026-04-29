43e34647fc7b269c95f263fa11ce25d26c69cd53
chimnayajith <chinmayajith30@gmail.com>
2026-04-29 20:59:27 +0530

    ls4q3: integrate cli-color for styled console output

--- lab4-node/server.js
+++ lab4-node/server.js
@@ -1,12 +1,22 @@
 const http = require("http");
 const greet = require("./greeting");
+const clc = require("cli-color");
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  const message = greet("Chinmay");
-  res.end(`Welcome to Node.js Server\n${message}`);
+  console.log(clc.blue("Request received"));
+
+  if (req.url === "/") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end(`Welcome to Node.js Server\n${greet("Chinmay")}`);
+    console.log(clc.green("Response sent successfully!"));
+  } else {
+    console.log(clc.red(`Error: Route ${req.url} not found`));
+
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("404 Not Found");
+  }
 });
 
 server.listen(3000, () => {
-  console.log("Server running at port 3000");
+  console.log(clc.green("Server running at port 3000"));
 });
